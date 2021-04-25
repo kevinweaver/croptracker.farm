101887243bd7c141e893385d482aac19a987ef72
Kevin Weaver <kevinweaver2@gmail.com>
2021-04-24 22:37:23 -0400

    Create home page

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -1,23 +1,13 @@
-import Image from 'next/image';
 
 export default function HomeHero() {
   return (
     <>
-      <Image
-        src="/images/kevin-mask.png" // Route of the image file
-        height={310} // Desired size with correct aspect ratio
-        width={270} // Desired size with correct aspect ratio
-        alt="Kevin W"
-        style="m-4"
-        className="kevin"
-      />
-
-              <h1>
-                <span className="">Hi. I like to</span>
-                  <span>{' '}make{' '}</span>
-                  <span className="">things</span>
-                  <span>.</span>
-              </h1>
+      <h1 className="text-center text-gray-900">
+        Tractor Supply Depot
+      </h1>
+      <h1 className="text-center text-9xl">
+          🚜
+      </h1>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import HomeHero from '../components/HomeHero.js'
 
 export default function Home() {
   return (
-    <div className="bg-cover bg-center" style={{backgroundImage: 'url(images/background.png)'}}>
+    <div className="bg-cover bg-center" style={{backgroundImage: 'url(images/farm.jpg)'}}>
         <Head>
           <title>Hi.</title>
           <link rel="icon" href="/favicon.ico" />
