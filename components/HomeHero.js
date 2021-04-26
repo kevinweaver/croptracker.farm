6ec67839659c3b2d58d8874ee7bbdd4149527521
Kevin Weaver <kevinweaver2@gmail.com>
2021-04-25 20:14:49 -0400

    Set up prettier

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -1,13 +1,13 @@
-import GasPrice from './GasPrice.js'
+import GasPrice from "./GasPrice.js";
 
 export default function HomeHero() {
   return (
     <div className="relative">
-      <div style={{marginTop: "10em"}}></div>
-      <h1 className="text-center text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
-          🚜
+      <div className="mt-10" style={{ marginTop: "10em" }}></div>
+      <h1 className="text-center text-6xl sm:text-7xl md:text-9xl lg:text-10xl">
+        🚜
       </h1>
-      <GasPrice/>
+      <GasPrice />
     </div>
-  )
+  );
 }
