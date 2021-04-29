873877fdeaae7f13197ad627c71854b6309c39d9
Kevin Weaver <kevinweaver2@gmail.com>
2021-04-29 00:42:40 -0400

    Return gasPrice() from zapper

--- components/utils/Zapper.js
+++ components/utils/Zapper.js
@@ -2,8 +2,9 @@ const apiKey = process.env.NEXT_PUBLIC_ZAPPER_API_KEY;
 const apiUrl = process.env.NEXT_PUBLIC_ZAPPER_API_URL;
 
 export default async function gasPrice() {
-  const requestUrl =
-    apiUrl + "/v1/gas-price?network=ethereum&api_key=" + apiKey;
+  const requestUrl = apiUrl + "v1/gas-price?network=ethereum&api_key=" + apiKey;
 
-  return await fetch(requestUrl);
+  const response = await fetch(requestUrl);
+  const prices = await response.json();
+  return prices;
 }
